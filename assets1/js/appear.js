1ba0611e43bf4bd43ee7e636baaa80bdf627fd12
Manikandan.R <manikandanmani69931@gmail.com>
2023-07-02 06:51:02 +0530

    upload

--- assets1/js/appear.js
+++ assets1/js/appear.js
@@ -118,3 +118,19 @@
   }
   return jQuery;
 }()));
+
+
+let popup = document.getElementsByClassName("popup-card")[0];
+let closeBtn = document.getElementById("close-btn");
+
+function showPopup() {
+  setTimeout(() => {
+    popup.style.transform = "translateY(0)";
+  }, 3000);
+}
+
+closeBtn.addEventListener("click", (e) => {
+  popup.style.transform = "translateY(-70vh)";
+});
+
+window.onload = showPopup();
